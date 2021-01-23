789eebae2de71c2eb0501981c62a4e51b3277bce
rrkumawatcst <rrkumawatcst@gmail.com>
2021-01-23 16:08:26 +0530

    update 1 introduction and test SQL database connection string

---
+++ GLP/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GLP.Models;
+
+namespace GLP.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewTestTable view = new ViewTestTable();
+            view.viewTestTable = new TestTable().getTestTable();
+            return View(view);
+        }
+    }
+}
---
+++ GLP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GLP.MvcApplication" Language="C#" %>
